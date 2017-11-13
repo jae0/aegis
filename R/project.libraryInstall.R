d2d720a38e814076ce20acb7da734edf208a3776
Jae S Choi <choijaeseok@gmail.com>
2017-11-12 22:55:34 -0400

    typo

--- R/project.libraryInstall.R
+++ R/project.libraryInstall.R
@@ -1,8 +1,8 @@
-project.libraryInstall = function(local=TRUE) {
+project.libraryInstall = function(local=TRUE,...) {
   #\\ add install_github flags e.g. force=TRUE to call if desired
     pkgsInstalled = .packages(all.available = TRUE)
     if ( ! "devtools" %in% pkgsInstalled ) install.packages( "devtools", dependencies=TRUE )
-    mylibs = project.libraryList()
+    mylibs = project.libraryList(project_root)
     if (local) {
       for ( pkg in mylibs$gitLoc ) {
          try( devtools::install_git( pkg, dependencies=FALSE, ... ) )
