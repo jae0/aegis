e25354e9c3001ee9f1f4a544dfcbe59fa94408b9
Jae S Choi <choijaeseok@gmail.com>
2017-11-12 22:50:26 -0400

    typo

--- R/project.libraryList.R
+++ R/project.libraryList.R
@@ -1,6 +1,6 @@
 
 
-project.libraryList = function() {
+project.libraryList = function(project_root) {
     mylibs = data.frame( rbind(
       cbind( "LaplacesDemon", file.path(project_root, "LaplacesDemon"),"jae0/LaplacesDemon" ),
       cbind( "LaplacesDemonCpp", file.path(project_root, "LaplacesDemonCpp"), "jae0/LaplacesDemonCpp"),
@@ -8,7 +8,7 @@ project.libraryList = function() {
       cbind( "emaf", file.path(project_root, "emaf"), "jae0/emaf"),
       cbind( "stm", file.path(project_root, "stm"), "jae0/stm"),
       cbind( "netmensuration", file.path(project_root, "netmensuration"), "jae0/netmensuration" ),
-      cbind( "bio.taxonomy", file.path(project_root, "bio.taxonomy"), , "jae0/bio.taxonomy"),
+      cbind( "bio.taxonomy", file.path(project_root, "bio.taxonomy"), "jae0/bio.taxonomy"),
       cbind( "bio.models", file.path(project_root, "bio.models"), "jae0/bio.models"),
       cbind( "bio.groundfish", file.path(project_root, "bio.groundfish"), "jae0/bio.groundfish"),
       cbind( "bio.snowcrab", file.path(project_root, "bio.snowcrab"), "jae0/bio.snowcrab")
