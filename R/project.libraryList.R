9ecfc6f73e7e5cc9bfacc47d7c299ca7ee996096
Jae S Choi <choijaeseok@gmail.com>
2017-11-22 09:34:49 -0400

    cleanup

--- R/project.libraryList.R
+++ R/project.libraryList.R
@@ -10,7 +10,6 @@ project.libraryList = function() {
       cbind( "netmensuration", file.path(code_root, "netmensuration"), "jae0/netmensuration" ),
       cbind( "bio.taxonomy", file.path(code_root, "bio.taxonomy"), "jae0/bio.taxonomy"),
       cbind( "bio.models", file.path(code_root, "bio.models"), "jae0/bio.models"),
-      cbind( "bio.groundfish", file.path(code_root, "bio.groundfish"), "jae0/bio.groundfish"),
       cbind( "bio.snowcrab", file.path(code_root, "bio.snowcrab"), "jae0/bio.snowcrab")
     ), stringsAsFactors=FALSE)
 
