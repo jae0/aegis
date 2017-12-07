ebc21b90510fa963772551abf1b0bffd4dc0c02d
Jae S Choi <choijaeseok@gmail.com>
2017-12-07 13:24:13 -0400

    clean up parallel.run call and semantics

--- R/parallel.run.r
+++ R/parallel.run.r
@@ -1,75 +1,65 @@
 #'  Run a parallel process .. wrapper for snow/parallel. Expectation of all relevant parameters in a list 'p'.
 
-parallel.run = function( FUNC, p, export=NULL, rndseed = 1, specific.allocation.to.clusters=F,... ) {
+parallel.run = function( FUNC, p, INDEX,  
+  clusters=NULL, clustertype=NULL, clusterexport=NULL, rndseed=NULL, ... ) {
 
   require(parallel)
 
-  res = with( p, {
-    if (!exists("clusters")) {
-      k = detectCores()
-      clusters = "localhost"
-      message( paste( "Using serial mode as no clusters were defined.", k, "cores are found on localhost, Define 'p$clusters' if you wish to run in parallel mode." ))
-    }
-    if (!exists("clustertype")) {
-      clustertype = "PSOCK"
-      message( paste( "Using", clustertype, "connections as default, 'clustertype' was not defined." ))
-    }
-    if (!exists("rndseed")) {
-      message( paste( "Using", rndseed, "as the default random number seed for parallel operations, Specify 'rndseed' to change." ))
-    }
-    if (!exists("nruns")) stop( "Must define 'nruns' in the paramater list")
+  nvars = length(INDEX)  # INDEX must be a list
+  p$runs = expand.grid(INDEX)
+  p$nruns = nrow( p$runs )
+  p$runs_uid = do.call(paste, c(p$runs, sep="~"))
+  
+  if (is.null(clusters)) if (exists("clusters", p)) clusters = p$clusters
+  if (is.null(clusters)) {
+    clusters = "localhost"
+    message( "'clusters' were not defined, using serial mode" )
+  }  
+  
+  if (is.null(clustertype)) if (exists("clustertype", p)) clustertype=p$clustertype 
+  if (is.null(clustertype)) {
+    clustertype = "PSOCK"
+    message( "'clustertype' was not defined, using PSOCK connections as default." )
+  }
+    
+  if (is.null(rndseed)) if (exists("rndseed", p)) rndseed=p$rndseed 
+  if (is.null(rndseed)) {
+      rndseed = 1
+      message( "'rndseed' was not defined, using rndseed=1 as the default." )
+  }
+    
+  message( "The processes are being run on:")
+  message(  paste( unlist( clusters), collapse=" ") )
 
-    message( "The processes are being run on:")
-    message(  paste( unlist( p$clusters), collapse=" ") )
-
-    if ( length(clusters) == 1 | nruns==1 ) {
-      out = NULL
-      out = suppressMessages( FUNC( p=p, ... ) )
-      return( out )
-    }
-
-    if ( nruns < length(clusters) ) clusters = sample( clusters, nruns )  # if very few runs, use only what is required
-
-    if ( length(clusters) > 1 ) {
-
-      cl = makeCluster( spec=clusters, type=clustertype ) # SOCK works well but does not load balance as MPI
-      RNGkind("L'Ecuyer-CMRG")  # multiple streams of pseudo-random numbers.
-
-      clusterSetRNGStream(cl, iseed=rndseed )
-
-      if ( !is.null(export)) clusterExport( cl, export )
-
-      # define allocation of runs to clusters
-      if ( !specific.allocation.to.clusters ) {
-        # default is a simple split
-        ssplt = lapply( clusterSplit( cl, 1:nruns ), function(i) i )
-      } else {
-        # split runs by species or area to collect analysis on a single aspect together for saving
-        uv = unique(p$runs$v)
-        uvl = length(uv)
-        lc = length(p$clusters)
-        lci = 1:lc
-        ssplt = list()
-        for(j in 1:uvl) {
-          ssplt[[j]]  = which(p$runs$v == uv[j])
-        }
-        ssplt2 = rep(list(numeric()),lc)
-        if(uvl>lc) {
-          for(j in 1:uvl) {
-            k=j
-            if(j>lc) k = j%%lc+1
-            ssplt2[[k]] <- c(ssplt2[[k]],ssplt[[j]])
-          }
-        }
-        ssplt = ssplt2
+  out = NULL
+  if ( length(clusters) == 1 | nruns==1 ) {
+    out = suppressMessages( FUNC( p=p, ... ) )
+  } else if ( nruns < length(clusters) ) {
+    clusters = sample( clusters, nruns )  # if very few runs, use only what is required
+  }
+  
+  if ( length(clusters) > 1 ) {
+    cl = makeCluster( spec=clusters, type=clustertype ) # SOCK works well but does not load balance as MPI
+    RNGkind("L'Ecuyer-CMRG")  # multiple streams of pseudo-random numbers.
+    clusterSetRNGStream(cl, iseed=rndseed )
+    if ( !is.null(clusterexport)) clusterExport( cl, clusterexport )
+    uv = unique(p$runs_uid)
+    uvl = length(uv)
+    lc = length(clusters)
+    lci = 1:lc
+    ssplt = list()
+    for(j in 1:uvl) ssplt[[j]]  = which(p$runs_uid == uv[j])
+    clustertasklist = rep(list(numeric()),lc)
+    if (uvl>lc) {
+      for(j in 1:uvl) {
+        k=j
+        if(j>lc) k = j%%lc+1
+        clustertasklist[[k]] <- c(clustertasklist[[k]],ssplt[[j]])
       }
-
-      out = NULL
-      out = suppressMessages( clusterApply( cl, ssplt, FUNC, p=p, ... ) )
-      stopCluster( cl )
-      return( out )
     }
-  })
-
-  return(  res  )
+    ssplt = NULL
+    out = suppressMessages( clusterApply( cl, clustertasklist, FUNC, p=p, ... ) )
+    stopCluster( cl )
+  }
+  return(  out  )
 }
