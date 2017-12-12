c0d51f84b5006c724a6f1fe6c8f82afe5b8b9f2a
Jae S Choi <choijaeseok@gmail.com>
2017-12-12 09:33:05 -0400

    create simple list

--- R/project.libraryList.R
+++ R/project.libraryList.R
@@ -1,6 +1,21 @@
 
 
-project.libraryList = function() {
+project.libraryList = function( DS="default" ) {
+  if (DS=="default") {
+    mylibs = data.frame( rbind(
+      cbind( "aegis.env", file.path(code_root, "aegis.env"), "jae0/aegis.env"),
+      cbind( "aegis", file.path(code_root, "aegis"), "jae0/aegis"),
+      cbind( "stm", file.path(code_root, "stm"), "jae0/stm"),
+      cbind( "netmensuration", file.path(code_root, "netmensuration"), "jae0/netmensuration" ),
+      cbind( "bio.taxonomy", file.path(code_root, "bio.taxonomy"), "jae0/bio.taxonomy"),
+      cbind( "bio.snowcrab", file.path(code_root, "bio.snowcrab"), "jae0/bio.snowcrab")
+    ), stringsAsFactors=FALSE)
+
+    names(mylibs) = c( "libname", "gitLoc", "githubLoc" )
+    return(mylibs)
+  }
+  
+  if (DS=="jae") {
     mylibs = data.frame( rbind(
       cbind( "LaplacesDemon", file.path(code_root, "LaplacesDemon"),"jae0/LaplacesDemon" ),
       cbind( "LaplacesDemonCpp", file.path(code_root, "LaplacesDemonCpp"), "jae0/LaplacesDemonCpp"),
@@ -13,8 +28,9 @@ project.libraryList = function() {
       cbind( "bio.models", file.path(code_root, "bio.models"), "jae0/bio.models"),
       cbind( "bio.snowcrab", file.path(code_root, "bio.snowcrab"), "jae0/bio.snowcrab")
     ), stringsAsFactors=FALSE)
-
     names(mylibs) = c( "libname", "gitLoc", "githubLoc" )
     return(mylibs)
+  }
+  
 }
 
