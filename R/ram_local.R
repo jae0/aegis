e12953088e721dc183ef19f2d849ffc072997159
Jae S Choi <choijaeseok@gmail.com>
2018-03-01 08:30:25 -0400

    add memory parsing

---
+++ R/ram_local.R
@@ -0,0 +1,29 @@
+ram_local = function( returnall=FALSE) {
+  os = Sys.info()[["sysname"]]
+  if (os == "Windows"){
+    x = try( memory.size(max = FALSE))
+    if ( "try-error" %in% class(x) ) {
+      x <- system2("wmic", args =  "OS get FreePhysicalMemory /Value", stdout = TRUE)
+      x <- x[grepl("FreePhysicalMemory", x)]
+      x <- gsub("FreePhysicalMemory=", "", x, fixed = TRUE)
+      x <- gsub("\r", "", x, fixed = TRUE)
+      x = as.integer(x)
+    }
+    return(x)
+  }
+
+  if (os=="Linux") {
+    x = system2('free', args='-m', stdout=TRUE)
+    y = x[ grepl("total", x) ]
+    y = unlist( strsplit( y, "[[:space:]]+") )
+    y[1] = "dummy"
+
+    z = x[ grepl("Mem", x) ]
+    z = unlist( strsplit( z, "[[:space:]]+") )
+    z[1] = NA
+    z = as.numeric(z)
+    names(z) = y
+    if (returnall) return(z)
+    return(z["total"])
+  }
+}
