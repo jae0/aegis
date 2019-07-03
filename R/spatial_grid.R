f0f3de8d57d1ce2831a5a53c7a01d1ff2b8234d0
Jae S Choi <choi.jae.seok@gmail.com>
2019-07-03 15:11:58 -0300

    reduce ram and speed optimizations

--- R/spatial_grid.R
+++ R/spatial_grid.R
@@ -2,21 +2,26 @@
 spatial_grid = function( p, DS="planar.grid", nm=NULL ) {
   #\\ grid locations
 
+
   if (DS=="planar.grid") {
     plon = seq(min(p$corners$plon), max(p$corners$plon), by=p$pres)
     plat = seq(min(p$corners$plat), max(p$corners$plat), by=p$pres)
-    g = expand.grid( plon, plat, KEEP.OUT.ATTRS=FALSE)
-    if (is.null(nm)) nm=c("plon", "plat")
-    names( g ) = nm
+    # g = expand.grid( plon, plat, KEEP.OUT.ATTRS=FALSE)
+    g = cbind(
+      plon = rep.int(plon, length(plat)),
+      plat = rep.int(plat, rep.int(length(plon),length(plat))))
+    if (!is.null(nm)) names( g ) = nm
     return(g)
   }
 
   if (DS=="lonlat.grid") {
     lons = seq(min(p$corners$lon), max(p$corners$lon), by=p$dres)
     lats = seq(min(p$corners$lat), max(p$corners$lat), by=p$dres)
-    g = expand.grid( lons, lats, KEEP.OUT.ATTRS=FALSE)
-    if (is.null(nm)) nm=c("lon", "lat")
-    names( g ) = nm
+    # g = expand.grid( lons, lats, KEEP.OUT.ATTRS=FALSE)
+    g = cbind(
+      lon = rep.int(lon, length(lat)),
+      lat = rep.int(lat, rep.int(length(lon),length(lat))))
+    if (!is.null(nm)) names( g ) = nm
     return(g)
   }
 
