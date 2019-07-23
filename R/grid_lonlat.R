016985a0e50d1db0853661ffb2ab425641105972
Jae S Choi <choi.jae.seok@gmail.com>
2019-07-23 12:35:11 -0300

    floor round issue

--- R/grid_lonlat.R
+++ R/grid_lonlat.R
@@ -2,8 +2,8 @@
   grid_lonlat = function (x, res=10) {
     multiplier = 100/res
     x = convert.degdec2degmin(x)
-    x$lon = trunc(x$lon * multiplier) / multiplier
-    x$lat = trunc(x$lat * multiplier) / multiplier
+    x$lon = floor(x$lon / multiplier + 1L) * multiplier
+    x$lat = floor(x$lat / multiplier + 1L) * multiplier
     x = convert.degmin2degdec(x)
     return(x)
   }
