e9ef23389e5ec4f5339962d99f8e1f31de1bb0cf
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-01 10:58:16 -0300

    refactor and simplify

---
+++ R/temporal_parameters.R
@@ -0,0 +1,47 @@
+
+temporal_parameters = function( p=NULL, ... ) {
+
+  if (is.null(p)) p=list()
+  p_add = list(...)
+  if ( length(p_add) > 0 ) p = c(p, p_add)
+  i = which(duplicated(names(p), fromLast=TRUE ))
+  if ( length(i) > 0 ) p = p[-i] # give any passed parameters a higher priority, overwriting pre-existing variable
+
+  # ----------------------------------
+  # determine problem's temporal dimensionality
+
+  if ( p$aegis_dimensionality == "space" ) {
+
+    #nothing else to do
+
+  } else if ( p$aegis_dimensionality == "space-year" ) {
+
+    if (!exists("yrs", p)) p$yrs = c(1999:lubridate::year(lubridate::now()))  # years for modelling and interpolation
+    p$ny = length(p$yrs)
+    if (!exists("nw", p)) p$nw = 10 # default value of 10 time steps number of intervals in time within a year for all temp and indicators
+    p$tres = 1/ p$nw # time resolution .. predictions are made with models that use seasonal components
+    p$dyears = (c(1:p$nw)-1) / p$nw # intervals of decimal years... fractional year breaks
+    p$dyear_centre = p$dyears[ round(p$nw/2) ] + p$tres/2
+    if (!exists("prediction_dyear", p)) p$prediction_dyear = lubridate::decimal_date( lubridate::ymd("0000/Sep/01")) # used for creating timeslices and predictions  .. needs to match the values in aegis_parameters()
+    if (!exists("nt", p)) p$nt = p$ny   # ie, default is an annual model (no p$nw)
+    if (!exists("prediction_ts", p)) p$prediction_ts = p$yrs + p$prediction_dyear # output timeslices for predictions in decimla years, yes all of them here
+
+  } else if ( p$aegis_dimensionality == "space-year-season" ) {
+
+    if (!exists("yrs", p)) p$yrs = c(1999:lubridate::year(lubridate::now()))  # years for modelling and interpolation
+    p$ny = length(p$yrs)
+    if (!exists("nw", p)) p$nw = 10 # default value of 10 time steps number of intervals in time within a year for all temp and indicators
+    p$tres = 1/ p$nw # time resolution .. predictions are made with models that use seasonal components
+    p$dyears = (c(1:p$nw)-1) / p$nw # intervals of decimal years... fractional year breaks
+    p$dyear_centre = p$dyears[ round(p$nw/2) ] + p$tres/2
+    if (!exists("prediction_dyear", p)) p$prediction_dyear = lubridate::decimal_date( lubridate::ymd("0000/Sep/01")) # used for creating timeslices and predictions  .. needs to match the values in aegis_parameters()
+    if (!exists("nt", p)) p$nt = p$nw*p$ny # i.e., seasonal with p$nw (default is annual: nt=ny)
+    if (!exists("prediction_ts", p)) {
+      # predictions at these time values (decimal-year), # output timeslices for predictions in decimla years, yes all of them here
+      tout = expand.grid( yr=p$yrs, dyear=1:p$nw, KEEP.OUT.ATTRS=FALSE )
+      p$prediction_ts = sort( tout$yr + tout$dyear/p$nw - p$tres/2 )# mid-points
+    }
+  }
+
+  return(p)
+}
