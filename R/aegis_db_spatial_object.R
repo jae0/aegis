edd91654c262fea340272c0a70b4e8c969b340ea
Jae S Choi <choi.jae.seok@gmail.com>
2019-11-12 07:43:47 -0400

    data inputs fix .. sp comp

--- R/aegis_db_spatial_object.R
+++ R/aegis_db_spatial_object.R
@@ -4,14 +4,19 @@ aegis_db_spatial_object = function( spatial_domain="SSE", proj4string="+proj=utm
   require(raster)
   # return base layout of aegis grids as a Spatial* object
   # bathymetry is the basis of all grids
-  bathymetry = bathymetry.db( DS="baseline", spatial_domain=spatial_domain )
+
+  pn = spatial_parameters( spatial_domain=spatial_domain )
+
+  bathymetry = bathymetry.db( p=pn, DS="complete" )
+  bathymetry = geo_subset( spatial_domain=spatial_domain, Z=bathymetry )
+
   spdf0 = SpatialPoints(bathymetry[, c("plon", "plat")], proj4string=sp::CRS(proj4string) )
 
   raster_template = raster(extent(spdf0)) # +1 to increase the area
   res(raster_template) = areal_units_resolution_km  # in units of crs (which should be in  km)
   crs(raster_template) = projection(spdf0) # transfer the coordinate system to the raster
 
-  spobj = rasterize( bathymetry[, c("plon", "plat")], raster_template )
+  spobj = rasterize( bathymetry[, c("plon", "plat")], raster_template, field=bathymetry$z )
   if ( returntype=="raster") return(spobj)
   spobj = as(spobj, "SpatialPixelsDataFrame")
 
