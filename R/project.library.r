150d861ef248169c32aa5aeb0de5cba3d794529b
Jae S Choi <choi.jae.seok@gmail.com>
2022-01-06 14:26:00 -0400

    cleanup

--- R/project.library.r
+++ R/project.library.r
@@ -1,40 +1,34 @@
-project.library = function( mylibs = NULL, ... ) {
+project.library = function( ... ) {
 
-  if (is.null(mylibs)) {
-    if (exists( "pkgs_local_git" )) {
-      mylibs = data.frame( libname=pkgs_local_git  )
-      mylibs$local = file.path(code_root, mylibs$libname) 
-      mylibs$github = paste( "jae0", mylibs$libname, sep="/") 
-      mylibs$bitbucket = paste( "ecomod", mylibs$libname, sep="/") 
+  # same as RLibrary
+  #\\ used to (re)load libraries conveniently
+
+  ll = unique(c(...))
+
+  pkgs = .packages(all.available = TRUE)
+  pkgsLoaded = .packages()
+
+  found = intersect( pkgs, ll )
+  if (length(found) > 0 ) {
+    for ( pkg in found ) {
+      # try( detach( paste("package", pkg, sep=":"), unload=TRUE, character.only=TRUE, force=TRUE ), silent=TRUE )
+      try ( suppressMessages( require( pkg, character.only = TRUE )), silent = TRUE  )
     }
   }
 
-  pkgsLoaded = .packages()
-  pkgsInstalled = .packages(all.available = TRUE)
-
-  ll = unique( c(...) )
-  found = intersect( pkgsInstalled, ll )
-  if (length(found) > 0 ) {
-    for ( pkg in found ) require( pkg, character.only = TRUE )
-  }
-
-  notfound = setdiff( ll, pkgsInstalled )
+  notfound = setdiff( ll, pkgs )
   if (length(notfound) > 0) {
-    print( "Missing some bio dependencies...")
-    n = readline(prompt="Install them? (local/github/no): ")
-    if (n %in% c("local", "github", "bitbucket") ) {
-      if (exists("mylibs")) {
-        for ( nf in notfound ) {
-          oo = which( mylibs$libname == nf )
-          if (n=="local") try( remotes::install_git( mylibs$git[oo[1]], dependencies=FALSE, ... ) )
-          if (n=="github") try( remotes::install_github( mylibs$github [oo[1]], ... ) )
-          if (n=="bitbucket") try( remotes::install_bitbucket( mylibs$bitbucket[oo[1]], ... ) )
-          pkg = mylibs$libname[oo]
-          require( pkg, character.only = TRUE )
-        }
+    print( "Missing some dependencies...")
+    print( notfound )
+    n = readline(prompt="Install them? (y/n): ")
+    if ( tolower(n) %in% c("y", "yes") ) {
+      for ( nf in notfound ) {
+        try( utils::install.packages( nf, dependencies=TRUE ) )
+        try( require( pkg, character.only = TRUE ) )
       }
     }
   }
+
   return( ll )
 }
 
