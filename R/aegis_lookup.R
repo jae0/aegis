17789e8e71ebd9d3a92a95735ac7375aaf26bdca
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-07 11:58:22 -0400

    cleanup

--- R/aegis_lookup.R
+++ R/aegis_lookup.R
@@ -228,43 +228,27 @@ aegis_lookup = function(
       if ( "bathymetry" %in% aegis_project ) {
         if ( is.null(pL) )  pL = bathymetry_parameters(  project_class=project_class  )
         if (is.null(space_resolution)) if (exists( "pres", pL)) space_resolution = pL$pres
-        if ( project_class %in% c("core" ) ) {
-          LUT = bathymetry_db ( p=pL, DS=DS ) 
-          if (output_format == "points" ) {
-            if ( space_resolution != pL$pres ) {
-            # regrid to another resolution
-              LUT$plon = trunc(LUT$plon / space_resolution + 1 ) * space_resolution
-              LUT$plat = trunc(LUT$plat / space_resolution + 1 ) * space_resolution
-              if (DS == "aggregated_data") {
-                setDT(LUT)
-                LUT = LUT[, .( z.mean = mean(z.mean, na.rm=TRUE) ), by=list(plon, plat) ]
-                setDF(LUT)
-              }
-            }
-          }
-        } 
+        if ( project_class %in% c("core" ) ) LUT = bathymetry_db ( p=pL, DS=DS ) 
         if ( project_class %in% c("stmv", "hybrid") ) LUT = bathymetry_db ( p=pL, DS="complete" )   
         if ( project_class %in% c("carstm" )) LUT = carstm_model( p=pL, DS="carstm_modelled_summary" ) 
+
+          if ( project_class %in% c("core", "stmv", "hybrid") )  {
+            setDT(LUT)
+            if ( space_resolution != pL$pres ) {
+              # regrid to another resolution
+              LUT$plon = trunc(LUT$plon / space_resolution + 1 ) * space_resolution
+              LUT$plat = trunc(LUT$plat / space_resolution + 1 ) * space_resolution
+              LUT = LUT[, setNames(.(mean( get(variable_name), na.rm=TRUE) ), variable_name), by=list(plon, plat) ]
+            }
+            setDF(LUT)
+          }
+
       }
 
       if ( "substrate" %in% aegis_project ) {
         if ( is.null(pL) )  pL = substrate_parameters(  project_class=project_class  )
         if (is.null(space_resolution)) if (exists( "pres", pL)) space_resolution = pL$pres
-        if ( project_class %in% c("core" ) ) {
-          LUT = substrate_db ( p=pL, DS=DS ) 
-          if (output_format == "points" ) {
-            if ( space_resolution != pL$pres ) {
-              # regrid to another resolution
-              LUT$plon = trunc(LUT$plon / space_resolution + 1 ) * space_resolution
-              LUT$plat = trunc(LUT$plat / space_resolution + 1 ) * space_resolution
-              if (DS == "aggregated_data") {
-                setDT(LUT)
-                LUT = LUT[, .( substrate.grainsize = mean(substrate.grainsize.mean, na.rm=TRUE) ), by=list(plon, plat) ]
-                setDF(LUT)
-              }
-            }
-          }
-        }  
+        if ( project_class %in% c("core" ) ) LUT = substrate_db ( p=pL, DS=DS ) 
         if ( project_class %in% c("stmv", "hybrid") ) {
           LUT = substrate_db ( p=pL, DS="complete" )   
           pB = bathymetry_parameters( spatial_domain=pL$spatial_domain, project_class=project_class  )
@@ -272,6 +256,18 @@ aegis_lookup = function(
           LUT = cbind( LUT, BA )
         }
         if ( project_class %in% c("carstm" )) LUT = carstm_model( p=pL, DS="carstm_modelled_summary" ) 
+                
+          if ( project_class %in% c("core", "stmv", "hybrid") )  {
+            setDT(LUT)
+            if ( space_resolution != pL$pres ) {
+              # regrid to another resolution
+              LUT$plon = trunc(LUT$plon / space_resolution + 1 ) * space_resolution
+              LUT$plat = trunc(LUT$plat / space_resolution + 1 ) * space_resolution
+              LUT = LUT[, setNames(.(mean( get(variable_name), na.rm=TRUE) ), variable_name), by=list(plon, plat) ]
+            }
+            setDF(LUT)
+          }
+
       }
 
       if ( "temperature" %in% aegis_project ) {
@@ -279,29 +275,27 @@ aegis_lookup = function(
         if (is.null(pL) )  pL = temperature_parameters(  project_class=project_class, year.assessment=year.assessment )
         if (is.null(space_resolution)) if (exists( "pres", pL)) space_resolution = pL$pres
         if (is.null(time_resolution))  if (exists( "tres", pL)) time_resolution =  pL$tres
-        if ( project_class %in% c("core" ))  {
-          LUT = temperature_db ( p=pL, DS=DS )  # "aggregated_data", "bottom.all"
-          if (output_format == "points" ) {
+        if ( project_class %in% c("core" ))  LUT = temperature_db ( p=pL, DS=DS )  # "aggregated_data", "bottom.all"
+        if ( project_class %in% c("stmv", "hybrid") )  LUT = temperature_db ( p=pL, DS="complete" ) 
+        if ( project_class %in% c("carstm" )) LUT = carstm_model( p=pL, DS="carstm_modelled_summary" ) 
+        
+          if ( project_class %in% c("core", "stmv", "hybrid") )  {
+            setDT(LUT)
+            if ( time_resolution != pL$tres ) {
+              LUT$dyear = trunc(LUT$dyear / time_resolution + 1 ) * time_resolution
+            }
             if ( space_resolution != pL$pres ) {
               # regrid to another resolution
               LUT$plon = trunc(LUT$plon / space_resolution + 1 ) * space_resolution
               LUT$plat = trunc(LUT$plat / space_resolution + 1 ) * space_resolution
-              LUT$dyear = trunc(LUT$dyear / time_resolution + 1 ) * time_resolution
-              if (DS == "aggregated_data") {
-                setDT(LUT)
-                LUT = LUT[, .( t.mean = mean(t.mean, na.rm=TRUE) ), by=list(plon, plat, yr, dyear) ]
-                setDF(LUT)
-              }
-              if (DS == "bottom.all") {
-                setDT(LUT)
-                LUT = LUT[, .( t = mean(t, na.rm=TRUE) ), by=list(plon, plat, yr, dyear) ]
-                setDF(LUT)
-              }
             }
+            if ( time_resolution != pL$tres |  space_resolution != pL$pres ) {
+              LUT = LUT[, setNames(.(mean( get(variable_name), na.rm=TRUE) ), variable_name), by=list(plon, plat, yr, dyear) ]
+              LUT$timestamp = lubridate::date_decimal( LUT$yr+LUT$dyear, tz=tz )
+            }
+            setDF(LUT)
           }
-        } 
-        if ( project_class %in% c("stmv", "hybrid") )  LUT = temperature_db ( p=pL, DS="complete" ) 
-        if ( project_class %in% c("carstm" )) LUT = carstm_model( p=pL, DS="carstm_modelled_summary" ) 
+    
       }
 
 
@@ -315,23 +309,26 @@ aegis_lookup = function(
         if (is.null(pL) )  pL = speciescomposition_parameters(  project_class=project_class, variabletomodel=sc_vn, year.assessment=year.assessment   )
         if (is.null(space_resolution)) if (exists( "pres", pL)) space_resolution = pL$pres
         if (is.null(time_resolution))  if (exists( "tres", pL)) time_resolution =  pL$tres
-        if ( project_class %in% c("core" ) ) {
-          LUT = speciescomposition_db ( p=pL, DS=DS )  
-          if (output_format == "points" ) {
+        if ( project_class %in% c("core" ) ) LUT = speciescomposition_db ( p=pL, DS=DS )  
+        if ( project_class %in% c( "stmv", "hybrid") )  LUT = aegis_db( p=pL, DS="complete" )   
+        if ( project_class %in% c("carstm" )) LUT = carstm_model( p=pL, DS="carstm_modelled_summary" ) 
+
+          if ( project_class %in% c("core", "stmv", "hybrid") )  {
+            setDT(LUT)
+            if ( time_resolution != pL$tres ) {
+              LUT$dyear = trunc(LUT$dyear / time_resolution + 1 ) * time_resolution
+            }
             if ( space_resolution != pL$pres ) {
               # regrid to another resolution
               LUT$plon = trunc(LUT$plon / space_resolution + 1 ) * space_resolution
               LUT$plat = trunc(LUT$plat / space_resolution + 1 ) * space_resolution
-              LUT$dyear = trunc(LUT$dyear / time_resolution + 1 ) * time_resolution
-              setDT(LUT)
+            }
+            if ( time_resolution != pL$tres |  space_resolution != pL$pres ) {
               LUT = LUT[, setNames(.(mean( get(sc_vn), na.rm=TRUE) ), sc_vn), by=list(plon, plat, yr, dyear) ]
               LUT$timestamp = lubridate::date_decimal( LUT$yr+LUT$dyear, tz=tz )
-              setDF(LUT)
             }
+            setDF(LUT)
           }
-        }
-        if ( project_class %in% c( "stmv", "hybrid") )  LUT = aegis_db( p=pL, DS="complete" )   
-        if ( project_class %in% c("carstm" )) LUT = carstm_model( p=pL, DS="carstm_modelled_summary" ) 
 
       }
 
@@ -424,11 +421,11 @@ aegis_lookup = function(
 
         variable_name = intersect( names(LUT), variable_name )
         LUT = LUT[, variable_name]
-
         LU_map =  st_points_in_polygons( pts=LUT, polys=LOCS_AU[, "AUID"], varname= "AUID" ) 
         if (!exists("AUID", LOCS ))  LOCS[["AUID"]]  = st_points_in_polygons( pts=LOCS, polys=LOCS_AU[, "AUID"], varname= "AUID" ) 
 
-        LUT = setDT(st_drop_geometry(LUT))
+        st_geometry(LUT) = NULL
+        setDT(LUT)
         for (vnm in variable_name) {
           if ( vnm %in% names(LUT )) {  
             # LUT_regridded = tapply( st_drop_geometry(LUT)[, vnm], LU_map, FUN=FUNC, na.rm=TRUE )
