e077f2b9d3b01925a7e34111ba80cebfc9c7c60a
Jae S Choi <choi.jae.seok@gmail.com>
2022-02-14 14:55:54 -0400

    cleanup

--- R/filter_data.R
+++ R/filter_data.R
@@ -1,5 +1,6 @@
 
 filter_data = function( Y, selection ) {
+  data.table::setDT(Y)
   tokeep = 1:nrow(Y)
   nm = names(selection)
   nn = length(nm)
@@ -10,25 +11,25 @@ filter_data = function( Y, selection ) {
       if (exists( "ranged_data", selection)) {
         if ( mm %in% selection$ranged_data ) { # ranged variables
           rgs = range( selection[[mm]], na.rm=TRUE)
-          tokeep = intersect( tokeep, which( Y[,mm] >= rgs[1] & Y[,mm] <= rgs[2] ) )
+          tokeep = intersect( tokeep, which( Y[,..mm] >= rgs[1] & Y[,..mm] <= rgs[2] ) )
           next()
         }
       }
       if (exists( "less_than", selection)) {
         if ( mm %in% selection$less_than ) { # ranged variables
           dd =  selection[[mm]]
-          tokeep = intersect( tokeep, which( Y[,mm] < dd ) )
+          tokeep = intersect( tokeep, which( Y[,..mm] < dd ) )
           next()
         }
       }
       if (exists( "greater_than", selection)) {
         if ( mm %in% selection$greater_than ) { # ranged variables
           dd =  selection[[mm]]
-          tokeep = intersect( tokeep, which( Y[,mm] > dd ) )
+          tokeep = intersect( tokeep, which( Y[,..mm] > dd ) )
           next()
         }
       }
-      tokeep = intersect( tokeep, which( Y[,mm] %in% selection[[mm]] ) )  # exact matches otherwise as default
+      tokeep = intersect( tokeep, which( Y[,..mm] %in% selection[[mm]] ) )  # exact matches otherwise as default
     }
   }
   return ( sort(unique(tokeep)) )
