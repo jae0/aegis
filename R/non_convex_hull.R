1f26a5810e2e789292e3904a6c148aaaf249de7d
Jae S Choi <choijaeseok@gmail.com>
2023-01-10 12:15:44 -0400

    more control of hull function

--- R/non_convex_hull.R
+++ R/non_convex_hull.R
@@ -1,25 +1,41 @@
 
 
-non_convex_hull = function( xy, alpha=NULL, plot=FALSE ) {
-  #\\ using the technique from https://rpubs.com/geospacedman/alphasimple
-  #\\ find the outline of a cloud of points
-  #\\ see stmv::ah2sp
+non_convex_hull = function( xy, alpha=NULL, plot=FALSE, dres=NULL ) {
   
-  sf_triangles = st_collection_extract( st_triangulate(do.call(c, st_geometry(xy) )  ))
+  if( is.null(dres)) {
+    # best that xy be planar 
+    bb = sf::st_bbox( xy)
+    xr = bb["xmax"] - bb["xmin"]
+    yr = bb["xmax"] - bb["ymin"]
+    dres = round( min(c(xr, yr)) / 1000 ) 
+  }
+  
+  xy$uid = 1:nrow(xy)
+  xy_raster = stars::st_rasterize( xy["uid"], dx=dres, dy=dres )
+  xy_pts = sf::st_as_sf( xy_raster, as_points=TRUE, na.rm=FALSE )
+  xy_pts = xy_pts[ which(is.finite(xy_pts$uid)), ]
+  st_crs(xy_pts) = st_crs( xy ) 
+ 
+  sf_triangles = st_collection_extract( st_triangulate(do.call(c, st_geometry(xy_pts) )  ))
 
   if (is.null(alpha) ) {
     lens = st_length(st_cast(sf_triangles, "LINESTRING"))
-    alpha = quantile(lens, 0.99)
+    alpha = max( quantile(lens, 0.95), dres*10 )
   }
 
-  sf_triangles = sf_triangles[lens <= alpha]
-  sfu = st_union( sf_triangles )
-  sfu = st_buffer( sfu, dist=alpha/5 ) 
+  sfu = sf_triangles[lens <= alpha]
+  sfu = st_buffer( sfu, dist=alpha * 1.5   ) 
+  sfu = st_union( sfu )
+  
   return ( sfu )
 
   # old method .. 
   use_alpha_hull = FALSE
   if (use_alpha_hull ) {
+    #\\ using the technique from https://rpubs.com/geospacedman/alphasimple
+    #\\ find the outline of a cloud of points
+    #\\ see stmv::ah2sp
+
     require(alphahull)
     require(igraph)
     o = ashape( xy[,1], xy[,2], alpha=alpha )
