94c60cd05381fbea867e132226480e2fb1809933
Jae S Choi <choijaeseok@gmail.com>
2023-07-03 06:40:11 -0300

    improve boundary

--- R/non_convex_hull.R
+++ R/non_convex_hull.R
@@ -60,12 +60,12 @@ non_convex_hull = function( xy, plot=FALSE, lengthscale=NULL, method="voronoi" )
  
     sfu = sf_triangles[ which(lens <= lenll) ]
     sfu = st_union( sfu )
-    sfu = st_buffer( sfu, dist=lengthscale*1.25 )  
+    sfu = st_buffer( sfu, dist=lengthscale*1.5 )  
     sfu = st_union( sfu )
     sfu = st_cast(sfu, "POLYGON")
     sfu = st_cast(sfu[1], "LINESTRING")
     sfu = st_cast(sfu[1], "POLYGON")
-    sfu = st_buffer( sfu, dist=lengthscale )  
+    sfu = st_buffer( sfu, dist=lengthscale*1.5 )  
     sfu = st_make_valid(sfu)
     
     return ( sfu )
