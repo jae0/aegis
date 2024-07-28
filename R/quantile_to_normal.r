51b6f433e662c39102b6f61f1401d4a1065eb2ed
Jae S Choi <choijaeseok@gmail.com>
2024-07-28 10:39:16 -0300

    more comments

--- R/quantile_to_normal.r
+++ R/quantile_to_normal.r
@@ -1,13 +1,14 @@
-#' convert quantile to normal with mean 0.5 and variance such that ci% are found within the range (0,1)
 
 quantile_to_normal = function( x, mean =0.5, ci=0.999 ) {
+  # zscore transform but constrained to have mean 0.5 and ci% prob range between 0,1
+  #' convert quantile to normal with mean 0.5 and variance such that ci% are found within the range (0,1)
 
   xmin = min( x[ which(x>0) ], na.rm=T )
   xmax = max( x[ which(x<1) ], na.rm=T )
 
-  pr = {1 - ci} / 2  # one-tailed prob in normal approximation desired
+  pr = {1 - ci} / 2  # two-tailed prob in normal approximation desired
   zsd = 2 * abs( qnorm( pr, mean=0, sd=1) )  # range in x required to have ci% of the population, two tailed
-  rsd = 1 / zsd # sd required for population to fit in (0,1)
+  rsd = 1 / zsd # sd required for "normal" population to fit in (0,1)
 
   zscore = qnorm( x, mean=mean, sd=rsd )
 
@@ -16,6 +17,11 @@ quantile_to_normal = function( x, mean =0.5, ci=0.999 ) {
 
   nn = which( x<=0 | zscore < 0)
   if (length(nn) > 0 ) zscore[ nn ] = 0
+
+  attr(zscore, "zsd") = zsd
+  attr(zscore, "rsd") = rsd
+  attr(zscore, "rmean") = mean
+  attr(zscore, "ci") = ci
 
   return (zscore)
 }
