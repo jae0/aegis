da6b252eb8ce08e001b84e4189fbd1f319240e23
jae0 <2574393+jae0@users.noreply.github.com>
2025-12-11 14:01:07 -0400

    revert show_image

--- R/show_image.R
+++ R/show_image.R
@@ -3,7 +3,8 @@ show_image = function( fn ){
   par(mar=c(0.5, 0.5, 0.5, 0.5))
   o = png::readPNG(fn)
   res = dim(o)[2:1] # get the resolution, [x, y]
-  # plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',
-  #  xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
+  # rasterImage requires a plot first
+  plot(1,1,xlim=c(1,res[1]),ylim=c(1,res[2]),asp=1,type='n',
+    xaxs='i',yaxs='i',xaxt='n',yaxt='n',xlab='',ylab='',bty='n')
   rasterImage(o, 1, 1, res[1], res[2])
 } 
