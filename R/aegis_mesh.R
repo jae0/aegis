355d178b0371bcf9201df29b130089fad3e9eccb
jae0 <2574393+jae0@users.noreply.github.com>
2025-12-14 21:29:16 -0400

    remove spbuffer

--- R/aegis_mesh.R
+++ R/aegis_mesh.R
@@ -33,7 +33,7 @@ aegis_mesh = function( pts, boundary=NULL, hull_boundary_ratio=0.1, resolution=1
     res = aegis_mesh( pts=pts, hull_lengthscale=25, resolution=1, output_type="grid" )
     res = aegis_mesh( pts=pts, hull_lengthscale=25, resolution=1, output_type="grid.count" )
     res = aegis_mesh( pts=pts, hull_lengthscale=25, resolution=1 )
-    res = aegis_mesh( pts=pts, hull_lengthscale=25, resolution=5 areal_units_constraint_ntarget=1 )
+    res = aegis_mesh( pts=pts, hull_lengthscale=25, resolution=5, areal_units_constraint_ntarget=1 )
 
     mypalette = colorRampPalette(c("darkblue","blue3", "green", "yellow", "orange","red3", "darkred"), space = "Lab")(100)
 
